f749dabe61149c78a4f978c96065281f11734503
ellis <elrestea@gmail.com>
2026-01-16 15:56:05 +0000

    add grid and box logic

--- eas-script.js
+++ eas-script.js
@@ -1,6 +1,25 @@
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    document.querySelector('grid-container').appendChild(div);
 
+
+let x = 512;
+// container width and height
+
+let y = 16;
+// number of squares per side
+//THIS WILL BE MODIFIED WITH THE SLIDER
+
+let squareSize = x / y;
+// size of each square
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+
+    square.style.width = squareSize + 'px';
+    square.style.height = squareSize + 'px';
+    square.style.boxSizing = 'border-box';
+    square.style.border = '1px solid lightgray';
+   
+    document.querySelector('.grid-container').appendChild(square);
+
+    
 }
 
